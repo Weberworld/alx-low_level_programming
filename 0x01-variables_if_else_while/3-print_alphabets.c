8c16d4454ab68a126e4c860b2395ed2eeccf8e1b
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-16 16:51:32 -0700

    Upper and Lower case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all lower and upper case alphabets
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char upper[26] = "ABCDEFGHIJKLMNOPQRSTUWXYZ";
+	char lower[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		char l_char = lower[i];
+
+		putchar(l_char);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		char u_char = upper[i];
+
+		putchar(u_char);
+	}
+	putchar('\n');
+	return (0);
+}

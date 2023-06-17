2e311ec077eefde162e1bd637015fd76d1a2cbab
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-16 17:27:40 -0700

    All digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print all digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}

222856f28a72e9bc21bbe765b95c6447a57c97e8
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-20 10:59:36 -0700

    Print lowercase

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,20 @@
  * Return: Always 0 (success)
  */
 int printPutchar(void);
+
+/**
+ * _putchar - Writes the char c to the stdo
+ * @c: The character to be writed out
+ *
+ * Return: On success 1
+ */
+void _putchar(char c);
+
+/**
+ * print_alphabet - Funtion protoype
+ *
+ * Description: Prints all alphabets in lowercase
+ *
+ * Return: Void
+ */
+void print_alphabet(void);

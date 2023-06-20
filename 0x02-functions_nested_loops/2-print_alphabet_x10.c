853423f2efbf07d15881ebd60ee5bdbb5846fc7f
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-20 11:16:13 -0700

    Print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_alphabet - Void function
+ *
+ * Description: Prints all alphabets in lowercases
+ *
+ * Return: Void
+ */
+void  print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+}
+
+/**
+ * print_alphabet_x10 - Void Fucntion
+ *
+ * Description: Calls the print_alphabet func 10 times
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}

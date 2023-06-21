67764b962f60f353d85665d7101175341b068632
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-20 17:50:24 -0700

    Starts from

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Prints from a number to 98
+ * @n: Number to start from
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+	int counter;
+
+	for (counter = n; counter <= 98; counter++)
+	{
+		if (counter / 10 > 0)
+		{
+			_putchar((counter / 10) + '0');
+			_putchar((counter % 10) + '0');
+		}
+		else
+		{
+			_putchar((counter % 10) + '0');
+		}
+	}
+}

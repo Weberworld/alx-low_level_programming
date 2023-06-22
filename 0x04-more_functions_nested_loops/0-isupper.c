f63e6ff41018573ccd8494255584a0c3110ae266
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-22 12:30:21 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * _isupper - Functions that check ifa char is an uppercase
+ * @c: character to be checked
+ *
+ * Return: 1 if it is upper and 0 if not
+ */
+int _isupper(int c)
+{
+	if ((c >= 65)
+		||
+		(c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}

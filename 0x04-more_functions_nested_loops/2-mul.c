c64da7988831916ef5f32d7dd58a09c8053a5874
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-22 12:53:48 -0700

    Multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers
+ * @a: first paramter
+ * @b: Second number
+ *
+ * Return: the product of the numbers
+ *
+ */
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,3 +5,5 @@ void _putchar(char);
 int _isupper(int);
 
 int _isdigit(int);
+
+int mul(int int);

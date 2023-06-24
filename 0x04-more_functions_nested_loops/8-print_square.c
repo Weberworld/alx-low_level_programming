b8da44a5f2a1d288c181a929357ad4160163e9de
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-24 01:07:13 -0700

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square of # using a given size
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int row, col;
+
+	for (col = 0; col < size; col++)
+	{
+		for (row = 0; row < size; row++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}

07c976d9f9a4dd576b78686eb818c3a42a6559ea
Weberworld <omotosoboluwatife23@gmail.com>
2023-06-30 11:23:09 -0700

    Str cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenates two strings
+ * @dest: First str
+ * @src: Str that will be joined to the first one
+ *
+ * Return: The jointed string as one string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void _putchar(char);
+
+char *_strcat(char a, char b);

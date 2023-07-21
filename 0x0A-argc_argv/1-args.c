19555e0ae566a842142e6ac690b3c92dbf020af1
Weberworld <omotosoboluwatife23@gmail.com>
2023-07-21 09:53:31 -0700

    narg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry pont
+ * @argc: No of args
+ * @argv: Str passed
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}

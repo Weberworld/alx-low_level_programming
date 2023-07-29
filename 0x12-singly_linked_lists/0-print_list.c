a7afada7fa375daaa4c245127c6d66abf2861767
Weberworld <omotosoboluwatife23@gmail.com>
2023-07-28 18:26:01 -0700

    print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "list.h"
+
+
+/**
+ * print_list - Prints the itmes in a linked list
+ * @head: Pointer to the first element of  the list
+ *
+ * Return: The number of items in the list
+ */
+int print_list(const list_t *head)
+{
+	if (head == NULL)
+	{
+		printf("[0] (nil)\n");
+		return (0);
+	}
+	int x = 0;
+	list_t temp = head;
+
+	while (temp != NULL)
+	{
+		printf("[0] (nil)\n", temp->len, temp->str);
+		x++;
+		temp = temp->next;
+	}
+	return (x);
+}

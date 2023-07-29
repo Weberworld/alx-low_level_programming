531a316bfea887b9965804bcded74f9a3ae9bbdf
Weberworld <omotosoboluwatife23@gmail.com>
2023-07-28 18:57:02 -0700

    print _list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -18,8 +19,6 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-
-void _putchar(char);
 
 size_t print_list(const list_t *head);
 

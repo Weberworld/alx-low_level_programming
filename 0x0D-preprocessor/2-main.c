14eb2b8f2ed137ff4f2af302401159addd33dc7c
Weberworld <omotosoboluwatife23@gmail.com>
2023-09-12 06:28:02 -0700

    Directives

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

8c3d03bad8a04c1dbb9a6b9faa08d887efc982a1
Weberworld <omotosoboluwatife23@gmail.com>
2023-09-12 07:24:30 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _STRUCT_FILE
+#define _STRUCT_FILE
+
+#endif
+
+/**
+ * struct dog - A c Struct
+ * @name: *char type
+ * @age : type float
+ * @owner: type *char
+ */
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};

ef18986af41ab179410acb1fa6f12f3b0e6ad968
Weberworld <omotosoboluwatife23@gmail.com>
2023-09-12 07:32:35 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,18 @@
 #ifndef _STRUCT_FILE
 #define _STRUCT_FILE
 
-#endif
-
 /**
  * struct dog - A c Struct
  * @name: *char type
  * @age : type float
  * @owner: type *char
+ *
+ * Description: struct named "dog" itwhich stores its name, age and owner
  */
 struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
